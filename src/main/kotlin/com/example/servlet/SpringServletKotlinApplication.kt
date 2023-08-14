bcd4af5a9ecc936a1b46d88b3174a67b01a288d5
givxq <coooeeeeep@gmail.com>
2023-08-14 14:23:47 +0900

    add HelloServlet

--- src/main/kotlin/com/example/servlet/SpringServletKotlinApplication.kt
+++ src/main/kotlin/com/example/servlet/SpringServletKotlinApplication.kt
@@ -2,7 +2,9 @@ package com.example.servlet
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.boot.web.servlet.ServletComponentScan
 
+@ServletComponentScan
 @SpringBootApplication
 class SpringServletKotlinApplication
 
